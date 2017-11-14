0684f2c22e955de9c96206ae5cf0ffcc4526c03e
viperassassin <biploveansh72@gmail.com>
2017-11-14 19:39:09 +0530

    done

---
+++ details.py
@@ -0,0 +1,8 @@
+
+print('!!!WELCOME TO THE WORLD OF SPY LIKE JAMES BOND 2K17')
+spy_name =raw_input ("Enter spy name:")
+spy_age = input("Enter the age of spy:")
+spy_review = input("Enter the review:")
+
+
+
--- spychat.py
+++ spychat.py
@@ -1,3 +1,25 @@
-import spychat
-engine =spychat.init()
-engine.say("welcome to spychat....")
+from details import spy_name, spy_age , spy_review
+print('name of spy:' + spy_name)
+print('age:' + str(spy_age))
+print('review of spy:' + str(spy_review))
+spy_age = 0
+spy_review = 0.0
+spy_initial=raw_input("Enter whether are you Mr. and Ms.:")
+print('Initial of Spy:' + spy_initial)
+name = spy_initial + spy_name
+print('your full name:' + name)
+if(len(spy_name) > 0):
+ print('so your name is valid')
+ if(spy_age>10) and (spy_age<50):
+     print("You are authenticated User:" + name)
+     if(spy_review>4.5) and (spy_review<5.0):
+         print('Great Ace')
+     elif(spy_review>4.0) and (spy_review<4.5):
+         print('excellent')
+     elif(spy_review>3.5) and (spy_review<4.0):
+         print('perfect')
+     elif(spy_review>3.0) and (spy_review<3.5):
+         print('average')
+     else:
+         print('lol spy')
+print("!!Congrats you are authenticated user!!")
