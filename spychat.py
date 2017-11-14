dd71e8e3f30bc0856b1c90dd9629f8402876ab6f
viperassassin <biploveansh72@gmail.com>
2017-11-14 20:41:33 +0530

    main

--- spychat.py
+++ spychat.py
@@ -9,17 +9,17 @@ print('Initial of Spy:' + spy_initial)
 name = spy_initial + spy_name
 print('your full name:' + name)
 if(len(spy_name) > 0):
- print('so your name is valid')
- if(spy_age>10) and (spy_age<50):
-     print("You are authenticated User:" + name)
-     if(spy_review>4.5) and (spy_review<5.0):
-         print('Great Ace')
-     elif(spy_review>4.0) and (spy_review<4.5):
-         print('excellent')
-     elif(spy_review>3.5) and (spy_review<4.0):
-         print('perfect')
-     elif(spy_review>3.0) and (spy_review<3.5):
-         print('average')
-     else:
-         print('lol spy')
+    print('so your name is valid')
+    if(spy_age>10) and (spy_age<50):
+         print("You are authenticated User:" + name)
+         if(spy_review>4.5) and (spy_review<5.0):
+             print('Great Ace')
+         elif(spy_review>4.0) and (spy_review<4.5):
+            print('excellent')
+         elif(spy_review>3.5) and (spy_review<4.0):
+            print('perfect')
+         elif(spy_review>3.0) and (spy_review<3.5):
+            print('average')
+         else:
+            print('lol spy')
 print("!!Congrats you are authenticated user!!")
